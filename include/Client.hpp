93c4eb1b7dc4ebba9213544c47e5708e52ee22b0
0liwsko <oliwia.dziegiel6@gmail.com>
2026-05-04 23:57:16 +0200

    Add Client class with socket and message handling

--- include/Client.hpp
+++ include/Client.hpp
@@ -1,4 +1,27 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
+#include <string>
+
+class User;
+class CryptoManager;
+
+class Client {
+private:
+    int socket;
+    User* user;
+    CryptoManager* crypto;
+
+public:
+    Client(int socket, User* user);
+
+    void connectToServer();
+    void sendMessage(const std::string& message);
+    void receiveMessage(const std::string& message);
+
+    std::string encryptMessage(const std::string& message);
+    std::string decryptMessage(const std::string& message);
+
+};
+
 #endif // CLIENT_HPP
