3d20af40efe0909feb64c7db0705a6833d916b33
Leon <leon.pisarik@gmail.com>
2026-05-19 12:29:03 +0200

    małe zmiany w CryptoManager

--- include/CryptoManager.hpp
+++ include/CryptoManager.hpp
@@ -4,15 +4,12 @@
 #include <vector>
 #include <string>
 
-class CryptoManager {
-public:
+namespace CryptoManager {
     std::vector<std::string> generateRSAKeys();
     std::vector<unsigned char> encryptRSA(std::string msg);
     std::string decryptRSA(std::vector<unsigned char> msg);
     std::vector<unsigned char> encryptAES(std::string msg);
     std::string decryptAES(std::vector<unsigned char> msg);
-private:
-
 };
 
 #endif // CRYPTOMANAGER_HPP
